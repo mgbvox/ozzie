1e6d63131420eb7638e2cfbfd3032f830acd74d8
Matthew Billman <mgbvox@Matthews-MacBook-Pro.local>
2019-06-26 14:45:10 -0400

    update

--- recursive_permutations.py
+++ recursive_permutations.py
@@ -1,10 +1,10 @@
-def permutations(v, is_top = True):
+def permutations(v):
     if len(v) == 0 or len(v) == 1:
         return [v]
     p_list = []
     for idx,l in enumerate(v):
         rest = [j for i,j in enumerate(v) if i != idx]
-        perms = permutations(rest, is_top=False)
+        perms = permutations(rest)
         for perm in perms:
             p_list.append([l]+perm)
     return p_list
